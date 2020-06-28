9ce2bdf602d1045e09906bb6089d8223dd21ecbb
rituraj@helixteamhub.cloud <riturajborgohain@gmail.com>
2020-06-28 22:14:55 +0530

    add

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -51,6 +51,8 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
+        'student' => \App\Http\Middleware\RedirectIfNotStudent::class,
+        'student.guest' => \App\Http\Middleware\RedirectIfStudent::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,7 +46,28 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
+        $this->mapStudentRoutes();
+
         //
+    }
+
+    /**
+     * Define the "student" routes for the application.
+     *
+     * These routes all receive session state, CSRF protection, etc.
+     *
+     * @return void
+     */
+    protected function mapStudentRoutes()
+    {
+        Route::group([
+            'middleware' => ['web', 'student', 'auth:student'],
+            'prefix' => 'student',
+            'as' => 'student.',
+            'namespace' => $this->namespace,
+        ], function ($router) {
+            require base_path('routes/student.php');
+        });
     }
 
     /**
