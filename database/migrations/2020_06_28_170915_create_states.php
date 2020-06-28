accf763ac1b3c40d0f925d725af6398af251f777
rituraj@helixteamhub.cloud <riturajborgohain@gmail.com>
2020-06-28 22:45:58 +0530

    db created

---
+++ database/migrations/2020_06_28_170915_create_states.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStates extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('states', function (Blueprint $table) {
+            $table->increments('id'); 
+            $table->string('name',300); 
+            $table->boolean('is_delete')->default(0); 
+            $table->boolean('is_active')->default(1); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('states');
+    }
+}
